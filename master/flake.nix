8eb2297c48d2de9acb70737d6393ea84180b75fa
hugosenari <hugosenari@gmail.com>
2022-03-22 04:32:45 -0300

    chore: re index nimsnappy

--- master/flake.nix
+++ master/flake.nix
@@ -10,7 +10,7 @@
   inputs.src-nimsnappy-master.flake = false;
   inputs.src-nimsnappy-master.ref   = "refs/heads/master";
   inputs.src-nimsnappy-master.owner = "dfdeshom";
-  inputs.src-nimsnappy-master.repo  = "nimsnappy.git";
+  inputs.src-nimsnappy-master.repo  = "nimsnappy";
   inputs.src-nimsnappy-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
